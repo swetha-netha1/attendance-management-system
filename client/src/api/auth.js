6a0412c709dcb2fca1c4bfe7e70eb1ebd1209e64
swetha-netha1 <151846316+swetha-netha1@users.noreply.github.com>
2025-05-16 17:38:25 +0530

    Update auth.js

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL = 'https://attendance-management-system-xxgc.onrender.com';
 
 export const signup = async (email, password) => {
   const response = await axios.post(`${API_URL}/signup`, { email, password });
